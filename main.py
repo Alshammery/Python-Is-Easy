a4583ebe53a52c16e9f82d88e8be6627df3bd998
Alshammery <46838043+Alshammery@users.noreply.github.com>
2019-01-19 19:10:37 +0300

    Add files via upload
    Homework Assignment #1: Variables

---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 12:57:25 2019
+
+@author: alshammery
+Homework Assignment #1: Variables
+
+Song Characteristics Program
+"""
+
+# ---------------------------------------------------------------------
+# Song string variables
+# ---------------------------------------------------------------------
+
+# Define string variables
+SongName="Take Five"
+Artist="Dave Brubeck"
+Genre="Jazz"
+Lyrics="No lyrics"
+LyricsLanguage="No language"
+LyricsLink="No link"
+YouTubeLink="https://www.youtube.com/watch?v=vmDDOFXSgAs"
+
+# Print string variables
+print("Song Name:          ",SongName)
+print("Artist Name:        ",Artist)
+print("Genre Type:         ",Genre)
+print("Lyrics:             ",Lyrics)
+print("Lyrics Language:    ",LyricsLanguage)
+print("Lyrics Link:        ",LyricsLink)
+print("YouTube Link:       ",YouTubeLink)
+
+# ---------------------------------------------------------------------
+# Song integer variables
+# ---------------------------------------------------------------------
+
+# Define integer variables
+DurationInSeconds=328
+
+# Print integer variables
+print("Duration:           ",DurationInSeconds," Seconds")
+
+# ---------------------------------------------------------------------
+# Song float variables
+# ---------------------------------------------------------------------
+
+# Define float variables
+Price=5.99
+
+# Print float variables
+print("Song Price:         ",Price,"$")
+
+
+# -------------------------------END------------------------------------
